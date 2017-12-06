6b308ab025216054a438cd76e57d9096dee6e4db
Nils Bergmann <Nils1794@gmail.com>
2017-12-06 18:39:27 +0100

    Day 4

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -9,11 +9,17 @@ import itertools
 def solve2(lines):
     count = 0 
     for line in lines:
-        for wordA, wordB in itertools.product(line, line):
-            if sorted(wordA) == sorted(WordB):
+        isValid = True
+        for wordA, wordB in itertools.combinations(line, 2):
+            if sorted(wordA) == sorted(wordB):
+                isValid = False
+        if isValid:
+            count += 1
+    print("Solve 1:", count)
 
 _input = open("Day4/Input.txt", "r")
 lines = []
 for line in _input: 
     lines.append(line.split())
 solve1(lines)
+solve2(lines)
